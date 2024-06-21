a451a54be7f316805aa771685460526fcfb5f2f2
EvertonWallace <everton542@hotmail.com>
2024-06-20 21:53:10 -0300

    modificacao na getImage para apresentar a imagem no navegador

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,4 +1,4 @@
-const { gfs } = require('../models');
+const { gfs } = require('../config/db.js');
 const mongoose = require('mongoose');
 const GridFsStorage = require('multer-gridfs-storage');
 const multer = require('multer');
@@ -6,34 +6,37 @@ require("dotenv").config();
 
 
 const storage = new GridFsStorage({
-    url:`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.yeoaway.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`    ,
+    url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.yeoaway.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster1`,
     file: (req, file) => {
-        return {
-            bucketName: 'uploads', 
-            filename: file.originalname
-        };
-    }
-});
-
-const upload = multer({ storage });
-
-const uploadImage = (req, res) => {
+      return {
+        bucketName: 'uploads',
+        filename: file.originalname,
+      };
+    },
+  });
+  
+  const upload = multer({ storage });
+  
+  const uploadImage = (req, res) => {
     res.json({ file: req.file });
-};
-
-const getImage = async (req, res) => {
+  };
+  
+  const getImage = async (req, res) => {
     try {
-        const file = await gfs.files.findOne({ filename: req.params.filename });
-        if (!file) {
-            return res.status(404).json({ message: 'Arquivo não encontrado' });
-        }
-
-        const readstream = gfs.createReadStream({ filename: file.filename });
-        readstream.pipe(res);
+      const file = await gfs.files.findOne({ filename: req.params.filename });
+      if (!file) {
+        return res.status(404).json({ message: 'Arquivo não encontrado' });
+      }
+  
+      const readstream = gfs.createReadStream({ filename: file.filename });
+  
+      res.set('Content-Type', file.contentType);
+      readstream.pipe(res);
     } catch (error) {
-        res.status(500).json({ message: 'Erro ao recuperar imagem', error });
+      res.status(500).json({ message: 'Erro ao recuperar imagem', error });
     }
-};
+  };
+  
 
 module.exports = {
     uploadImage,
