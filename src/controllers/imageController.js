874c4ca23e61f25096b0f204fe184495cb27ec01
EvertonWallace <everton542@hotmail.com>
2024-06-20 21:31:11 -0300

    criacao da funcao getImage para recuperacao de uma imagem do GridFS pelo nome do arquivo

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -21,9 +21,22 @@ const uploadImage = (req, res) => {
     res.json({ file: req.file });
 };
 
+const getImage = async (req, res) => {
+    try {
+        const file = await gfs.files.findOne({ filename: req.params.filename });
+        if (!file) {
+            return res.status(404).json({ message: 'Arquivo não encontrado' });
+        }
 
+        const readstream = gfs.createReadStream({ filename: file.filename });
+        readstream.pipe(res);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao recuperar imagem', error });
+    }
+};
 
 module.exports = {
     uploadImage,
+    getImage,
     upload
 };
